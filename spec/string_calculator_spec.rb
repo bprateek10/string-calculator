5401603cd907f41a7bba945de74fca0d62bb1842
Prateek Bhatnagar <bprateek10@yahoo.in>
2024-05-19 12:53:50 +0530

    raise error on consecutive delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -15,11 +15,23 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.new.add(nil)).to eq(0)
       end
     end
-    
+
     context 'given n number' do
       it 'returns the sum of the numbers' do
         expect(StringCalculator.new.add('1,2,3,4')).to eq(10)
       end
     end
-  end
+
+    context 'given n number with new lines' do
+      it 'returns the sum of the numbers' do
+        expect(StringCalculator.new.add("1\n2,3")).to eq(6)
+      end
+    end
+
+    context 'Consecutive delimiters' do
+      it 'raises an ArgumentError' do
+        expect { StringCalculator.new.add("1,\n") }.to raise_error(ArgumentError, "Invalid input")
+      end
+    end
+  end  
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -2,6 +2,18 @@ class StringCalculator
   def add( nums )
     return 0 if nums.nil? || nums.empty?
     
-    return nums.split(',').map(&:to_i).reduce(:+)
+    if nums.match(/,\n|\n,|,,|\n\n/)
+      raise ArgumentError, "Invalid input"
+    end
+    
+    nums = nums.split(/,|\n/)
+    
+    sum = 0
+    
+    nums.each do |num|
+      sum += num.to_i
+    end
+    
+    sum
   end
 end
