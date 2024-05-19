19d35afaa74ff9345b630751ae613111aef2fa68
Prateek Bhatnagar <bprateek10@yahoo.in>
2024-05-19 13:23:51 +0530

    supporting custom delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,32 +6,45 @@ RSpec.describe StringCalculator do
   describe '#add' do
     context 'given an empty string' do
       it 'returns zero' do
-        expect(StringCalculator.new.add('')).to eq(0)
+        expect(subject.add('')).to eq(0)
       end
     end
 
     context 'given nil' do
       it 'returns zero' do
-        expect(StringCalculator.new.add(nil)).to eq(0)
+        expect(subject.add(nil)).to eq(0)
       end
     end
 
-    context 'given n number' do
+    context 'given a string with valid numbers and default delimiters' do
       it 'returns the sum of the numbers' do
-        expect(StringCalculator.new.add('1,2,3,4')).to eq(10)
+        expect(subject.add('1,2,3')).to eq(6)
+        expect(subject.add("1\n2,3")).to eq(6)
       end
     end
 
-    context 'given n number with new lines' do
+    context 'given a string with custom delimiter' do
       it 'returns the sum of the numbers' do
-        expect(StringCalculator.new.add("1\n2,3")).to eq(6)
+        expect(subject.add("//;\n1;2")).to eq(3)
+        expect(subject.add("//|\n1|2|3")).to eq(6)
+        expect(subject.add("//***\n1***2***3")).to eq(6)
       end
     end
 
-    context 'Consecutive delimiters' do
+    context 'given a string with consecutive default delimiters' do
       it 'raises an ArgumentError' do
-        expect { StringCalculator.new.add("1,\n") }.to raise_error(ArgumentError, "Invalid input")
+        expect { subject.add("1,\n") }.to raise_error(ArgumentError, "Invalid input")
+        expect { subject.add("1,\n2") }.to raise_error(ArgumentError, "Invalid input")
+        expect { subject.add('1,,2') }.to raise_error(ArgumentError, "Invalid input")
+        expect { subject.add("1\n\n2") }.to raise_error(ArgumentError, "Invalid input")
       end
     end
-  end  
+
+    context 'given a string with consecutive custom delimiters' do
+      it 'raises an ArgumentError' do
+        expect { subject.add("//;\n1;;2") }.to raise_error(ArgumentError, "Invalid input")
+        expect { subject.add("//|\n1||2") }.to raise_error(ArgumentError, "Invalid input")
+      end
+    end
+  end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,19 +1,23 @@
 class StringCalculator
-  def add( nums )
+  def add(nums)
     return 0 if nums.nil? || nums.empty?
-    
-    if nums.match(/,\n|\n,|,,|\n\n/)
+
+    delimiter = /,|\n/
+    if nums.start_with?('//')
+      parts = nums.split("\n", 2)
+      delimiter = Regexp.escape(parts[0][2..-1])
+      nums = parts[1]
+    end
+
+    # Check for invalid input: consecutive delimiters
+    if nums.match(/#{delimiter}{2,}|,{2,}|\n{2,}/)
       raise ArgumentError, "Invalid input"
     end
-    
-    nums = nums.split(/,|\n/)
-    
-    sum = 0
-    
-    nums.each do |num|
-      sum += num.to_i
-    end
-    
+
+    nums = nums.split(/#{delimiter}|,|\n/)
+
+    sum = nums.reduce(0) { |acc, num| acc + num.to_i }
+
     sum
   end
 end
