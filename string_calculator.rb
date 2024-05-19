65862ff9c76c600dd24b93c87ac312158752d5c9
Prateek Bhatnagar <bprateek10@yahoo.in>
2024-05-19 13:47:41 +0530

    refactor class

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,29 +1,54 @@
+require 'pry'
+
 class StringCalculator
   def add(nums)
-    return 0 if nums.nil? || nums.empty?
+    return 0 if empty_input?(nums)
 
+    delimiter = determine_delimiter(nums)
+    validate_consecutive_delimiters(nums, delimiter)
+    nums_array = extract_numbers(nums, delimiter)
+    validate_numbers(nums_array)
+
+    sum_numbers(nums_array)
+  end
+
+  private
+
+  def empty_input?(nums)
+    nums.nil? || nums.empty?
+  end
+
+  def determine_delimiter(nums)
     delimiter = /,|\n/
     if nums.start_with?('//')
       parts = nums.split("\n", 2)
-      delimiter = Regexp.escape(parts[0][2..-1])
-      nums = parts[1]
+      custom_delimiter = parts[0][2..-1]
+      delimiter = Regexp.escape(custom_delimiter)
     end
+    delimiter
+  end
 
-    # Check for invalid input: consecutive delimiters
-    if nums.match(/#{delimiter}{2,}|,{2,}|\n{2,}/)
-      raise ArgumentError, "Invalid input"
+  def validate_consecutive_delimiters(nums, delimiter)
+    if nums.match(/#{delimiter}{2}/)
+      raise ArgumentError, 'Invalid input'
     end
+  end
 
-    nums_array = nums.split(/#{delimiter}|,|\n/)
-    
-    # Check for negative numbers
-    negatives = nums_array.select { |num| num.to_i < 0 }
+  def extract_numbers(nums, delimiter)
+    nums_array = nums
+      .split(/#{delimiter}|,|\n/)
+      .map(&:to_i)
+    nums_array
+  end
+
+  def validate_numbers(nums_array)
+    negatives = nums_array.select { |num| num < 0 }
     unless negatives.empty?
       raise ArgumentError, "Negative numbers not allowed: #{negatives.join(', ')}"
     end
+  end
 
-    sum = nums_array.reduce(0) { |acc, num| acc + num.to_i }
-
-    sum
+  def sum_numbers(nums_array)
+    nums_array.reduce(0, :+)
   end
 end
