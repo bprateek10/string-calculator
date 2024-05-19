52b00c564996ab39eae578d0f08ce19615f27d2c
Prateek Bhatnagar <bprateek10@yahoo.in>
2024-05-19 12:04:39 +0530

    handling empty and nil condition

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,5 +1,18 @@
 require_relative '../string_calculator'
 
 RSpec.describe StringCalculator do
-  pending
+  subject { described_class.new }
+
+  describe '#add' do
+    context 'given an empty string' do
+      it 'returns zero' do
+        expect(StringCalculator.new.add('')).to eq(0)
+      end
+    end
+    context 'given nil' do
+      it 'returns zero' do
+        expect(StringCalculator.new.add(nil)).to eq(0)
+      end
+    end
+  end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,2 +1,5 @@
 class StringCalculator
+  def add( nums )
+    return 0 if nums.nil? || nums.empty?
+  end
 end
